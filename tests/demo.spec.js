b0249599b5266837c38397f70389d6529b12440b
Khawar Khan <ma3533899@gmail.com>
2024-04-28 18:47:15 +0500

    Deleted unnecessary files

--- tests/demo.spec.js
+++
@@ -1,7 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('first test', async({page}) => {
-    await page.goto('http://localhost/web%20Applications%20with%20PHP/finalModule_Course1/Login.php');
-
-    await page.pause();
-})
